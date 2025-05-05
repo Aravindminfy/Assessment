b63b77caba727733d392b5a3cddea3cd8ee2aff1
Aravindminfy <aravind.appusamy@minfytech.com>
2025-05-05 14:58:50 +0530

    Adding command to the code

--- code.py
+++ code.py
@@ -3,12 +3,12 @@
 # Write a code to print the factorial number
 # Use function and add logic to the function 
 
-def factorial(n):
-    if n == 1 or n == 0:
+def factorial(n): 
+    if n == 1 or n == 0: # base condition
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n-1) # logic for factorial of an number
     
 number = 5
 result = factorial(number)
-print(f"Factorial of the number {number} is {result}")
+print(f"Factorial of the number {number} is {result}") # answer to the end user 
